b7e78202080b1148535b12804fbc62ab685b60d6
vasyl.pashko <vasyl.pashko@gmail.com>
2023-03-23 21:32:00 +0200

    completed code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  setTextColor(letterFromText: string, letterEntered: string): string {
+    if (!letterEntered) {
+      return 'pending';
+    }
+    return letterFromText === letterEntered ? 'correct' : 'incorect';
+  }
 }
